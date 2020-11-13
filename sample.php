646bf56967b4ce32712cb3469fc463c9a381b5fd
root <root@ip-172-31-22-69.us-east-2.compute.internal>
2020-11-13 17:30:21 -0600

    plz

--- sample.php
+++ sample.php
@@ -6,7 +6,7 @@
         <title>Paper Trail</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel='styles.css?version=20' href="styles.css">
+        <link rel='styles.css?version=4' href="styles.css">
     </head>
     <article>
         <div class="storyInfo">
